9bf8c7fa3a5af29d9dc71e8d50bbddef1bb263fc
gabigonzalz <gabrielagonzalezfrias@gmail.com>
2024-08-22 18:39:02 -0400

    Set up the OpenWeatherMap API

--- git_weathered.py
+++ git_weathered.py
@@ -1,6 +1,18 @@
+import argparse # Commands
+import pyfiglet # Ascii titles
+from simple_chalk import chalk # Colors
+import requests
+
 """
 This module provides functions to compile weather data from anywhere
 in the form of a CLI program.
 """
 
 print("This is my weather data compiler")
+
+# API key for OpenWeatherMap API
+API_KEY = "0ac74061ff0627cde608d666a3d35636"
+
+# Base URL for OpenWeatherMap API
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
