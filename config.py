15772258b0d5255143dbbfcd7cfc0368bc8b259b
Gabriela <119881768+gabigonzalz@users.noreply.github.com>
2024-08-26 19:46:45 -0400

    Create config.py
    example config.py (not working without your own api key)

---
+++ config.py
@@ -0,0 +1,7 @@
+"""Configuration Module for my API KEY and my OPENWEATHERMAP URL"""
+
+# API key for OpenWeatherMap API
+API_KEY = "your_api_key"
+
+# Base URL for OpenWeatherMap API
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
